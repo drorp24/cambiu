afae8a6d56d9b6ac64d19f2f6988094c759227a4
Chen Eshchar <eshcharc@gmail.com>
2014-12-29 17:22:42 +0200

    map info window

--- www/js/controller/main-controller.js
+++ www/js/controller/main-controller.js
@@ -1,6 +1,8 @@
 function mainController($scope, $state, exchanges) {
   this.toggleMapList = function() {
-    if($state.$current.name === 'home.map') {
+    $scope.isMap = $state.$current.name === 'home.map';
+
+    if($scope.isMap) {
       $state.go('home.list');
     } else {
       $state.go('home.map');
@@ -28,4 +30,31 @@ function mainController($scope, $state, exchanges) {
   });
 
   $scope.exchanges = exchanges;
+
+  $scope.currentExchange = {
+    rate: 0.64
+  };
+
+  $scope.conversion = {
+    inverse: false,
+    left: {
+      flag: 'us',
+      value: 1.00
+    },
+    right: {
+      flag: 'gb',
+      value: $scope.currentExchange.rate
+    }
+  };
+
+  $scope.switchSides = function () {
+    var temp = $scope.conversion.left;
+
+    $scope.conversion.left = $scope.conversion.right;
+    $scope.conversion.right = temp;
+
+    $scope.conversion.left.value = 1;
+    $scope.conversion.right.value = $scope.conversion.inverse ? 1/$scope.currentExchange.rate : $scope.currentExchange.rate;
+    $scope.conversion.inverse = !$scope.conversion.inverse;
+  };
 }
--- www/js/service/exchange-service.js
+++ www/js/service/exchange-service.js
@@ -8,6 +8,7 @@ function exchangeService() {
             latitude: 51.5060,
             longitude: 0.1260
           },
+          templateUrl: '/template/map-info-window.html',
           details: {
             title: 'London Exchange'
           }
@@ -18,11 +19,24 @@ function exchangeService() {
             latitude: 51.5032,
             longitude: 0.1232
           },
+          templateUrl: '/template/map-info-window.html',
           details: {
             title: 'Yorkshare Exchange'
           }
         }
       ];
+    },
+    get: function() {
+      return {
+        id: 0,
+        details: {
+          title: 'London Exchange',
+          rates: {
+            USDGBP: 1.5625,
+            ILSGBP: 6.11
+          }
+        }
+      };
     }
   };
   //return $resrouce('/exchange');
