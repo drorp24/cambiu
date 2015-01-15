9e510d03cceca96eb77f0f96bb0224d7edc80ec2
Chen Eshchar <eshcharc@gmail.com>
2015-01-15 09:52:46 +0200

    rails server added

---
+++ db/migrate/20150115075000_add_latitude_and_longitude_to_exchange.rb
@@ -0,0 +1,6 @@
+class AddLatitudeAndLongitudeToExchange < ActiveRecord::Migration
+  def change
+    add_column :exchanges, :latitude, :float
+    add_column :exchanges, :longitude, :float
+  end
+end
