464cc7a5aabd42b2fb6a16fe83af613cf139b1eb
Chen Eshchar <eshcharc@gmail.com>
2015-01-15 10:33:43 +0200

    create exchange model

--- server/app/admin/exchange.rb
+++ server/app/admin/exchange.rb
@@ -1,10 +1,9 @@
 ActiveAdmin.register Exchange do
 
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :name, :address, :latitude, :longitude, :country, :user_ratings, :opens, :closes
   #
   # or
   #
@@ -13,6 +12,18 @@ ActiveAdmin.register Exchange do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-
+  form do |f|
+    f.inputs 'Details' do
+      f.input :name
+      f.input :address
+      f.input :latitude
+      f.input :longitude
+      f.input :user_ratings
+      f.input :opens
+      f.input :closes
+      f.input :country, :as => :string
+    end
+    f.actions
+  end
 
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115075000) do
+ActiveRecord::Schema.define(version: 20150115081704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 20150115075000) do
     t.datetime "updated_at"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "country"
+    t.float    "user_ratings"
+    t.time     "opens"
+    t.time     "closes"
   end
 
 end
