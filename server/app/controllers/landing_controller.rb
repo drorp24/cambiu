36f7bcca1cabf0243a084c50b270ffa972c3bb14
Dror Poliak <drorp24@gmail.com>
2015-02-02 20:31:34 +0200

    renaming stylesheet environment to 'boots'

--- server/app/controllers/landing_controller.rb
+++ server/app/controllers/landing_controller.rb
@@ -1,12 +1,6 @@
 class LandingController < ApplicationController 
   
   def index
-    @rate = Rate.new   
-    @rates = Rate.all
   end
-  
-  def new
-    @rate = Rate.new   
-  end
-  
+    
 end
