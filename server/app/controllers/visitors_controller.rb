bbf16f444fffc716ebd75e082fd5c9c4d9281e15
Dror Poliak <drorp24@gmail.com>
2015-02-07 21:38:54 +0200

    mixpanel events

--- server/app/controllers/visitors_controller.rb
+++ server/app/controllers/visitors_controller.rb
@@ -1,12 +1,16 @@
 class VisitorsController < ApplicationController 
   
   def create
-    @visitor = Visitor.new(visitor_params)
-    @visitor.save
-    render layout: false
+    return unless visitor_params[:email]
+    @visitor = Visitor.find_or_create_by(email: visitor_params[:email])
+    @visitor.update(visitor_params) 
+    respond_to do |format|
+        format.js
+    end  
   end
 
   def visitor_params
     params.require(:visitor).permit!
-  end    
+  end
+      
 end
