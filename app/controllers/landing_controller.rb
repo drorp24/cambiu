de9b06d06ff9c319c9d4da0c34d31352cf8993cb
Dror Poliak <drorp24@gmail.com>
2015-02-08 03:45:03 +0200

    css changes

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController 
   
   def index
-    @visitor = Visitor.new(buy_cents: 1000, buy_currency: "USD", pay_currency: "GBP", pay_cents: Currency.new.exchange(1000, "USD", "GBP"))
+    @visitor = Visitor.new(buy_cents: 100000, buy_currency: "USD", pay_currency: "GBP", pay_cents: Currency.new.exchange(100000, "USD", "GBP"))
     render layout: 'boots'
   end
     
