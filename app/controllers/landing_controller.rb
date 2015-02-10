9ed535642d71de093076b4e1be39eae2329eee4c
Dror Poliak <drorp24@gmail.com>
2015-02-10 13:57:23 +0200

    time-based cache control for index

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,7 +3,15 @@ class LandingController < ApplicationController
   
   def index
     @visitor = Visitor.new(buy_cents: 100000, buy_currency: "USD", pay_currency: "GBP", pay_cents: Currency.new.exchange(100000, "USD", "GBP"))
+    expires_in 3.days, :public => true
     render layout: 'boots'
+=begin
+    if stale?(etag: @visitor, last_modified: Date.today)
+      respond_to do |format| 
+        format. html {render layout: 'boots'}
+      end
+    end
+=end
   end
     
 end
