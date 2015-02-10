198be92be60b9e70cf78a7a43280a55fbb1505f9
Dror Poliak <drorp24@gmail.com>
2015-02-10 14:09:52 +0200

    removing caches_page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.static_cache_control = "public, max-age=31536000"   # not passed to Cloudfront (probably since it doesnt go thru rails)
+  config.static_cache_control = "public, max-age=31536000"   # not passed to Cloudfront
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
