f18fba4810760f9f3b98b238d4f270b6baae03a5
Dror Poliak <drorp24@gmail.com>
2015-02-10 17:03:39 +0200

    stale? for 304 responses

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,15 +3,12 @@ class LandingController < ApplicationController
   
   def index
     @visitor = Visitor.new(buy_cents: 100000, buy_currency: "USD", pay_currency: "GBP", pay_cents: Currency.new.exchange(100000, "USD", "GBP"))
-    expires_in 3.days, :public => true
-    render layout: 'boots'
-=begin
-    if stale?(etag: @visitor, last_modified: Date.today)
+    expires_in 3.hours, :public => true
+    if stale?(etag: @visitor, last_modified: Date.new(2015, 1, 1))
       respond_to do |format| 
         format. html {render layout: 'boots'}
       end
     end
-=end
   end
     
 end
