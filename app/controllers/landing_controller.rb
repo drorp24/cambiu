e15783113263cb42ded08404a2403a0de8fbf789
Dror Poliak <drorp24@gmail.com>
2015-02-12 20:34:43 +0200

    top5 preparation

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,12 +3,12 @@ class LandingController < ApplicationController
   
   def index
     @visitor = Visitor.new(buy_cents: 100000, buy_currency: "USD", pay_currency: "GBP", pay_cents: Currency.new.exchange(100000, "USD", "GBP"))
-    expires_in 3.hours, :public => true
-    if stale?(etag: @visitor, last_modified: Date.new(2015, 1, 1))
+    expires_in 3.hours, :public => true if Rails.env.production?
+#    if stale?(etag: @visitor, last_modified: Date.new(2015, 1, 1)) 
       respond_to do |format| 
         format. html {render layout: 'boots'}
       end
-    end
+#    end
   end
     
 end
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,7 @@ class VisitorsController < ApplicationController
     return unless visitor_params[:email]
     @visitor = Visitor.find_or_create_by(email: visitor_params[:email])
     @visitor.update(visitor_params) 
+    # js response is for the client to invoke mixpanel. That's the only way to include geolocation data in the mixpanel.people.set
     respond_to do |format|
         format.js
     end  
