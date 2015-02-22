df14ba8522deb8536a31aa639b490dcae23fd20a
Dror Poliak <drorp24@gmail.com>
2015-02-22 11:15:05 +0200

    adding deleted files back to git

---
+++ app/assets/javascripts/active_admin.js
@@ -0,0 +1,32 @@
+//= require active_admin/base
+//= require jquery
+//= require best_in_place
+//= require jquery-ui
+//= require best_in_place.jquery-ui
+//= require jquery.purr
+//= require best_in_place.purr
+//= require turbolinks
+$(document).ready(function() {
+    function add_rate() {
+    var top_tr = $('#index_table_rates tbody').find('tr').first();
+    top_tr.prepend(top_tr.clone());
+    };
+
+  /* Activating Best In Place */
+  jQuery(".best_in_place").best_in_place();
+  jQuery('.best_in_place').bind("ajax:success", function () {jQuery(this).closest('tr').effect('highlight'); });
+  $(document).on('best_in_place:error', function(event, request, error) {
+    // Display all error messages from server side validation
+    response = $.parseJSON(request.responseText);
+    $.each(response['errors'], function(index, value) {
+      if(value.length > 0) {
+        if( typeof(value) == "object") {value = index + " " + value.toString(); }
+        var container = $("<span class='flash-error'></span>").html(value);
+        container.purr();
+      };
+    });
+  });
+
+
+
+});
