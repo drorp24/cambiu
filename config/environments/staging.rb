d06d4184cd00dddb2a9fce5cf396221a1e941a4a
Dror Poliak <drorp24@gmail.com>
2015-03-04 18:14:27 +0200

    new test env, facebook app and cloudfront dist

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
   config.static_cache_control = "public, max-age=31536000"   # not passed to Cloudfront
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
