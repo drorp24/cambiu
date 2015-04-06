37edc5926a819ca62e6c41cc0d87e99e9111343d
Dror Poliak <drorp24@gmail.com>
2015-04-06 22:46:31 +0300

    gtm events

--- app/assets/javascripts/pageload.js
+++ app/assets/javascripts/pageload.js
@@ -143,33 +143,6 @@ $(document).ready(function() {
     $('.make-switch').bootstrapSwitch();    
 
 
-    // Link analyses
-    mixpanel.track_links("a", "link", function(ele) { 
-    return { clicked: $(ele).attr('data-link') };
-    });
-
-
-    // Change analyses
-    $("[data-analyze='change']").change(function() {
-
-        var activity = $(this).attr('data-activity');
-        var key1 = $(this).attr('data-key1');
-        var val1 = $(this).attr('data-val1');
-        var key2 = $(this).attr('data-key2');
-        var val2 = $(this).attr('data-val2');
-        var key3 = $(this).attr('data-key3');
-        var val3 = $(this).val();
-        
-        var aa = {};
-        aa[key1] = val1;
-        aa[key2] = val2;
-        aa[key3] = val3;
-
-        ga('send', 'event', activity, val1, val2, val3);
-        mixpanel.track(activity, aa);
-    });
-
-
     // Google maps invoked from client so needs to read url params
     function getParameterByName(name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
