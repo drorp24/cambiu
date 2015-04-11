1e711211c93c7a49694981ad867954b2e52f7088
Dror Poliak <drorp24@gmail.com>
2015-04-11 18:23:22 +0300

    pick cloudFront url from ENV, set in heroku

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "http://d368eop2iyjvb5.cloudfront.net"
+  # config.action_controller.asset_host = "http://d368eop2iyjvb5.cloudfront.net"
+  config.action_controller.asset_host = ENV["CLOUDFRONT_DIST"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
