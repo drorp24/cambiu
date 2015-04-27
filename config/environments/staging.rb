1e3106772ead7cb1476952d1d13d87f673a8cafa
Dror Poliak <drorp24@gmail.com>
2015-04-27 22:52:40 +0300

    .

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.static_cache_control = "public, s-maxage=31536000, max-age=31536000"
 
