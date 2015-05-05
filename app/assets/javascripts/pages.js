6bde05fe76fe20105c2e3e440600b299b0d17a19
Dror Poliak <drorp24@gmail.com>
2015-05-06 01:29:27 +0300

    .

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,14 +5,21 @@ $(document).ready(function() {
         var old_page = $('.active.page');
         var new_page = $($this.data('href'));
 
+        // exchanges.js - record the exchange id on the '#getit_button' (the one with the href, triggering this event)
+        // id = $this.data('id');
+        // var results = $.grep(exchanges_array, function(e){ return e.id == id; });
+        // var exchange = results[0]
+        // now populate everything in the summary page and the voucher page taking values from exchange, e.g., exchange.address
+        // then continue below: hiding the old page and showing the new page.
+
+
         old_page.removeClass('active');
         old_page.hide();
         new_page.addClass('active');
-        new_page.show();
+        new_page.show();    // consider css transition (take it from collapse css transition)
 
         // also change hash and push to history
 
-//        return false;
     }))
 
 
