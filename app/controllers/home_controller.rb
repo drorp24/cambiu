d37b0edf088d66ce334eabca414a3cf15ce2ba03
Gady Schujovitzky <gaaady@gmail.com>
2015-05-21 16:56:22 +0300

    set caching to 2 hours

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
-class HomeController < ApplicationController 
+class HomeController < ApplicationController
 
-  before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
-  caches_action :index                                      # quicker first rendering (pick-up ready page from cache)
-  
+  # before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
+  caches_action :index, expires_in: 2.hours, :race_condition_ttl => 20.seconds                                      # quicker first rendering (pick-up ready page from cache)
+
   def index
   end
-  
+
 end
