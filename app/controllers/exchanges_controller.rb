0431efbce5c73e27b6f80132bf836ade2f2095e7
Dror Poliak <drorp24@gmail.com>
2015-06-03 17:05:04 +0300

    remove action caching

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,7 +1,7 @@
 class ExchangesController < ApplicationController 
 
-  before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
-  caches_action :index                                      # quicker first rendering (pick-up ready page from cache)
+#  before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
+#  caches_action :index                                      # quicker first rendering (pick-up ready page from cache)
  
   def index
     @user = User.new
