aacea84723216e0873ca713bb63cbfa82cc4c522
Dror Poliak <drorp24@gmail.com>
2015-06-03 20:31:10 +0300

    Revert "remove action caching"
    This reverts commit 0431efbce5c73e27b6f80132bf836ade2f2095e7.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController 
 
-#  before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
-#  caches_action :index                                      # quicker first rendering (pick-up ready page from cache)
+  before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
+  caches_action :index                                      # quicker first rendering (pick-up ready page from cache)
   
   def index
   end
