1c1cf45530b9e7a6471368e0b2b03d1d4688cc62
Dror Poliak <drorp24@gmail.com>
2015-06-15 17:23:07 +0300

    separate exchanges/:id to different layout/js manifest, controller, route
    (still with all the js of the search spa)

---
+++ app/assets/javascripts/exchanges_manifest.js
@@ -0,0 +1,23 @@
+// APPLICATION.JS
+// Use only required bootstrap js. Order: https://github.com/twbs/bootstrap-sass/blob/master/assets/javascripts/bootstrap-sprockets.js 
+// Files that complement DOM building are first
+// Files for triggers and user interaction are last
+//
+
+//= require jquery
+//= require jquery_ujs
+//= require autonumeric
+//= require jquery.validate
+
+//= require pageload
+//= require search
+//= require location
+//= require models
+//= require exchanges
+//= require validations
+//= require pages
+
+//= require bootstrap/transition
+//= require bootstrap/collapse
+//= require bootstrap/carousel
+//= require bootstrap-switch
--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,8 +1,16 @@
 class ExchangesController < ApplicationController 
 
+=begin
   before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
   caches_action :index                                      # quicker first rendering (pick-up ready page from cache)
- 
+
+=end
+  def show
+    @exchange = Exchange.new
+  end
+
+
+=begin
   def index
     @user = User.new
   end
@@ -18,7 +26,8 @@ class ExchangesController < ApplicationController
     pay = exchange.quote(params[:buy_amount], params[:buy_currency], params[:pay_currency])
     render json: pay
   end
-  
+=end
+
   def exchange_params
     params.require(:exchange).permit!
   end
