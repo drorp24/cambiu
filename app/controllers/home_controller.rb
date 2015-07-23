9fedec9e23c21cc54b90c75419f8bae9b55a618a
Dror <drorp24@gmail.com>
2015-07-23 12:32:23 +0300

    remove action caching from home_c

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   # before_action :set_http_cache_headers, only: [:index]     # quicker next rendering  (return 304 instead of page)
-  caches_action :index, expires_in: 2.hours, :race_condition_ttl => 20.seconds                                      # quicker first rendering (pick-up ready page from cache)
+#  caches_action :index, expires_in: 2.hours, :race_condition_ttl => 20.seconds                                      # quicker first rendering (pick-up ready page from cache)
 
   def index
     if request.domain == 'currency-net.com'
