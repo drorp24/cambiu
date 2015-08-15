da1802e5a2b0876c9af86cc5fbc5e0bc83e912a1
Dror <drorp24@gmail.com>
2015-08-15 23:17:28 +0300

    .

--- app/assets/javascripts/pageload.js
+++ app/assets/javascripts/pageload.js
@@ -271,7 +271,7 @@ $(document).ready(function() {
 
     $(function () {
         $("body").tooltip({ selector: '[data-toggle=tooltip]' });
-        $("body").popover({ selector: '[data-toggle=popover]' });
+        if (mobile) $("body").popover({ selector: '[data-toggle=popover]' });
     });
 
     inform = function(title, text, hide) {
