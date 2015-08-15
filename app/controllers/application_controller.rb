8a8633e6727b3ace25782ed611787959eae8585c
Dror <drorp24@gmail.com>
2015-08-15 23:50:21 +0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,27 @@ class ApplicationController < ActionController::Base
 
   before_action :pass_request
 
+  before_action :detect_browser
+
+  private
+  def detect_browser
+    case request.user_agent
+      when /iPad/i
+        request.variant = :tablet
+      when /iPhone/i
+        request.variant = :phone
+      when /Android/i && /mobile/i
+        request.variant = :phone
+      when /Android/i
+        request.variant = :tablet
+      when /Windows Phone/i
+        request.variant = :phone
+      else
+        request.variant = :desktop
+    end
+  end
+
+
   def pass_request
     $request = request
   end
