e92f22a3ed77120c85a5077b589e3d0441e1e38a
Dror <drorp24@gmail.com>
2015-10-31 23:22:07 +0200

    .

--- app/assets/javascripts/exchanges.js
+++ app/assets/javascripts/exchanges.js
@@ -485,7 +485,7 @@ $(document).ready(function() {
                 map_center_changed = true;
 //                $('#directions-panel').css('display', 'block');
                 directionsDisplay.setDirections(response);
-                setTimeout(function(){ mapPan() }, 100);
+//                setTimeout(function(){ mapPan() }, 100);
 
             }
          });
