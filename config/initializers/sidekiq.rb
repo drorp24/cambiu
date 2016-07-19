bebb16534e9f2c6063aa24953d2099c6fe172c08
Dror <drorp24@gmail.com>
2016-07-19 15:50:36 +0300

    fix attempt to: ERR max number of clients reached (details)
    Warn: hardcode.  Change whenever either of the resources change

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISTOGO_URL'], network_timeout: 5 }
+  config.redis = { url: ENV['REDISTOGO_URL'], network_timeout: 5, size: 8 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISTOGO_URL"], network_timeout: 5 }
+  config.redis = { url: ENV["REDISTOGO_URL"], network_timeout: 5, size: 1 }
 end
