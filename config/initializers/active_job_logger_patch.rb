42e9eff07a44508d4f4babbd11949e43ad03a18b
Dror <drorp24@gmail.com>
2016-07-20 16:10:21 +0300

    cont

--- config/initializers/active_job_logger_patch.rb
+++ config/initializers/active_job_logger_patch.rb
@@ -1,5 +1,8 @@
-class ActiveJob::Logging::LogSubscriber
-  private def args_info(job)
-    ''
+ActiveSupport.on_load :active_job do
+  class ActiveJob::Logging::LogSubscriber
+    private
+    def args_info(job)
+      ''
+    end
   end
 end
