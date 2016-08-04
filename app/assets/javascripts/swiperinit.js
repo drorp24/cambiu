55d4a8224502441c14f65ca9c6ecdcd4838ddb89
Dror <drorp24@gmail.com>
2016-08-05 01:19:00 +0300

    Swipe up

--- app/assets/javascripts/swiperinit.js
+++ app/assets/javascripts/swiperinit.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
-    mySwiper = new Swiper ('.swiper-container', {
+    swiperH = new Swiper ('.swiper-container-h', {
+/*
         pagination: '.swiper-pagination',
         paginationType: 'fraction',
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
+ */
+
         centeredSlides: true,
-        /*
-         freeMode: true,
-         freeModeSticky: true,
-         */
         spaceBetween: 15,
         slidesPerView: 1.3
-//        effect: 'coverflow'
+     });
+    swiperV = new Swiper ('.swiper-container-v', {
+        direction: 'vertical',
+        slidesOffsetBefore: 150,
+        freeMode: true,
+        nextButton: '.swiper-button-next-v'//        nested: true
     });
+
     console.log('initialized Swiper');
 });
