857695a25c38581bbf369b4be851d39abd5c1593
Dror <drorp24@gmail.com>
2016-10-19 14:02:04 +0300

    reconstruct 'all assets control in one place'

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,18 @@
 # Be sure to restart your server when you modify this file.
 
+# Enable serving of images, stylesheets, and JavaScripts from an asset server.
+Rails.application.config.action_controller.asset_host = ENV["CLOUDFRONT_DIST"]
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-# Precompile fonts as well:
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
-# Add the fonts path:
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-# For devise on heroku
-Rails.application.config.assets.initialize_on_precompile = false
+# Compress JavaScripts and CSS.
+Rails.application.config.assets.compress = true
+Rails.application.config.assets.js_compressor = :uglifier
+Rails.application.config.assets.css_compressor = :sass
+
+# Do not fallback to assets pipeline if a precompiled asset is missed.
+Rails.application.config.assets.compile = false
+
+# Find precompilation dependencies in manifest.js
+Rails.application.config.assets.precompile = ["manifest.js"]
