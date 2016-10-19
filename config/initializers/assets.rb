af946dd6500aa25c60e575d634eade3f99a20047
Dror <drorp24@gmail.com>
2016-10-19 14:41:29 +0300

    assets.precompile +manifest -old enabled precompile to succeed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.action_controller.asset_host = ENV["CLOUDFRONT_DIST"]
+
 Rails.application.config.assets.version = '1.0'
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-# Precompile fonts as well:
-Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf)
-# Add the fonts path:
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-# For devise on heroku
-Rails.application.config.assets.initialize_on_precompile = false
+# Compress JavaScripts and CSS.
+
+# but precompile, based on manifest.js
+Rails.application.config.assets.precompile = ["manifest.js"]
