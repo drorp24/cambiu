62d55663e97223752b6b4d380740dda3202f2173
Dror <drorp24@gmail.com>
2016-10-19 14:06:28 +0300

    Revert "reconstruct 'all assets control in one place'"
    This reverts commit 857695a25c38581bbf369b4be851d39abd5c1593.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
+if Rails.env.development?
+#  Rails.logger = Le.new('e8b9f030-7290-48b3-852b-ee3903e9da2c', debug: true, :local => 'log/development.log')
+else
+  Rails.logger = Le.new(ENV['LOGENTRIES_TOKEN'])
+end
 
 # Initialize the Rails application.
 Rails.application.initialize!
