d70ec5f99fc863202ad98c50cf1b9aceb354fd33
Dror <drorp24@gmail.com>
2016-10-19 12:56:35 +0300

    Revert "."
    This reverts commit da22bd298bc391041ce22cb01d84297424b17ad2.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,7 @@
 //= require bootstrap/transition
 //= require bootstrap/collapse
 //= require bootstrap/carousel
+//= require star-rating
 //= require jquery-qrcode-0.14.0
 //= require sweetalert.min
 //= require swiper.jquery.min
