0a2f4bd9ca78dde13ee3640e515f176b36ba1137
Dror <drorp24@gmail.com>
2016-10-25 13:48:58 +0300

    desktop homepage video/text only

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,3 +1,3 @@
-Rack::Timeout.timeout = 30  # seconds
+Rack::Timeout.timeout = Rails.env.development? ? 90 : 30  # seconds
 Rack::Timeout::Logger.disable
 #Rack::Timeout.unregister_state_change_observer(:logger) if Rails.env.development?
