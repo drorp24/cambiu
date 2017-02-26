5eb98586560d45188ee9c97b3bb02b2dd1f3f7ca
Dror <drorp24@gmail.com>
2017-02-26 02:15:23 +0200

    self calculation of distance, no google

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -394,7 +394,7 @@ class Exchange < ActiveRecord::Base
 
     exchange_hash = {}
 
-    exchange_hash[:distance] = self.distance_from(center) # TODO: calculate it myself
+    exchange_hash[:distance] = self.alt_distance_from(center)
 
     quotes = quote(pay_amount: pay.amount, pay_currency: pay.currency.iso_code, get_amount: buy.amount, get_currency: buy.currency.iso_code, field: pay.amount > 0 ? 'pay_amount' : 'get_amount', radius: radius, distance: exchange_hash[:distance])
     return {} if quotes[:error].present? and !Rails.env.development?
@@ -457,6 +457,27 @@ class Exchange < ActiveRecord::Base
       return nil
     end
 
+  end
+
+  def alt_distance_from(center)
+
+    loc1 = [self.latitude, self.longitude]
+    loc2 = center
+
+    rad_per_deg = Math::PI/180  # PI / 180
+    rkm = 6371                  # Earth radius in kilometers
+    rm = rkm * 1000             # Radius in meters
+
+    dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+
+    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
+    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+
+    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+
+    rm * c / 1000 # Delta in Km
   end
 
   def fullname
