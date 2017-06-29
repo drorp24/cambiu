2d16dbb3debf5d38543b77b63645803012e2139f
Dror <drorp24@gmail.com>
2017-06-29 21:02:13 +0300

    .

--- app/assets/javascripts/flow.js
+++ app/assets/javascripts/flow.js
@@ -8,24 +8,24 @@
     console.log('flow');
 
     verifyUserLocation = getUserLocation();                // in case I need to verify location was found before doing something that requires it (e.g., search, particularly here below)
-    verifyUserLocation.then(doStuffThatRequiresLocation);  // that's the way to run multiple then's in parallel, and to separate one promise from a chain of promises
     //    if (value_of('search_id')) verifyUserLocation.then(search_and_show);
+
+
+$(document).ready(function() {  // TODO: only populate Params needs to wait to document. For the other (userLocation) it's just unneeded latency
+
+    verifyUserLocation.then(doStuffThatRequiresLocation);  // that's the way to run multiple then's in parallel, and to separate one promise from a chain of promises
+    setProperPage();
+
+});
 
 function doStuffThatRequiresLocation(location) {
 
     geocode(location);
     local = nearest_locale(location.lat, location.lng);
-    populateLocalCurrency(local);
+    populateParams(local);
     populateLocalBestRate(local);
 }
 
-
-$(document).ready(function() {  // TODO: only populate Params needs to wait to document. For the other (userLocation) it's just unneeded latency
-
-    populateParams();
-    setProperPage();
-
-});
 
 search_and_show = function(location) {
     return Promise.all([showMap(location), search()])
