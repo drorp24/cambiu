6930cd103ed1528bcbb5171b7cd3ea26b1e36b4d
Dror <drorp24@gmail.com>
2017-06-29 21:11:24 +0300

    Revert "Revert "Revert "."""
    This reverts commit c8263530a22462e2e3ae7a01f22dea3ac22aee89.

--- app/assets/javascripts/flow.js
+++ app/assets/javascripts/flow.js
@@ -8,24 +8,24 @@
     console.log('flow');
 
     verifyUserLocation = getUserLocation();                // in case I need to verify location was found before doing something that requires it (e.g., search, particularly here below)
-    //    if (value_of('search_id')) verifyUserLocation.then(search_and_show);
-
-
-$(document).ready(function() {  // TODO: only populate Params needs to wait to document. For the other (userLocation) it's just unneeded latency
-
     verifyUserLocation.then(doStuffThatRequiresLocation);  // that's the way to run multiple then's in parallel, and to separate one promise from a chain of promises
-    setProperPage();
-
-});
+    //    if (value_of('search_id')) verifyUserLocation.then(search_and_show);
 
 function doStuffThatRequiresLocation(location) {
 
     geocode(location);
     local = nearest_locale(location.lat, location.lng);
-    populateParams(local);
+    populateLocalCurrency(local);
     populateLocalBestRate(local);
 }
 
+
+$(document).ready(function() {  // TODO: only populate Params needs to wait to document. For the other (userLocation) it's just unneeded latency
+
+    populateParams();
+    setProperPage();
+
+});
 
 search_and_show = function(location) {
     return Promise.all([showMap(location), search()])
