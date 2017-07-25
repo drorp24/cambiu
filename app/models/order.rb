6e5f010e038317c191354e61247d02afb2f7f6ea
Dror <drorp24@gmail.com>
2017-07-25 23:43:54 +0300

    Revert "Merge remote-tracking branch 'origin/develop' into develop"
    This reverts commit 66383f1fdbc69e0cb8e93b22ccb79c83acfff99c, reversing
    changes made to 3d5608d9947292111b206641895547a1d3f35784.

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,25 +16,9 @@ class Order < ActiveRecord::Base
     self.service_type = 'pickup' unless self.service_type.present?
   end
 
-  after_update   :after_updating
+  after_commit   :notification
 
   attr_accessor :photo
-
-  def self.create_and_notify(params, language)
-    order = self.create!(params)
-    puts "about to notify - after create with:"
-    puts order.inspect
-    puts order.photo
-    puts language
-    NotifyJob.perform_later(order, order.photo, language) #if self.requires_notification?
-    order
-  end
-
-  def after_updating
-    puts "about to notify - after update"
-    NotifyJob.perform_later(self, self.photo, nil) #if self.requires_notification?
-  end
-
 
   def status_color
     [:orange, :green, :blue][Order.statuses[status]]
