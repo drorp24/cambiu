ad3f4437095b5c00504a761a4cb8807579e828a3
Dror <drorp24@gmail.com>
2017-09-11 23:38:21 +0300

    BE error reporting cache

--- app/models/error.rb
+++ app/models/error.rb
@@ -2,10 +2,14 @@ class Error < ApplicationRecord
   belongs_to :search
 
   def self.report(params)
+
     return unless params[:message].present? or params[:text].present?
-    puts "Error.report - called to report " + (params[:message] || "") + " - " + (params[:text] || "")
+    return if @last_message and (@last_message == params[:message])
+    @last_message = params[:message]
     return if Error.where(message: params[:message]).where("created_at >= ?", Time.zone.now.beginning_of_day).exists?
+
     Error.create(message: params[:message], text: params[:text], search_id: params[:search_id])
     ErrorJob.perform_later(params[:message], params[:text], params[:search_id])
+
   end
 end
