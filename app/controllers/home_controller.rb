0b2baf9e70ac4895ef0cdeddb921bf7500c91672
Dror <drorp24@gmail.com>
2017-09-21 17:13:28 +0300

    do not cache in development

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
-  caches_action :index, cache_path: :action_cache_key, unless: -> {Rails.env.development?}     # quicker first rendering (pick-up ready page from cache) - Dramatic effect.
-  before_action :set_http_cache_headers, only: [:index]                                         # quicker next rendering  (if challenged by browser with "If...", it quickly responds with 304 rather than generate a page)
+  caches_action :index, cache_path: :action_cache_key,
+                unless: -> {Rails.env.development?}           # quicker first rendering (pick-up ready page from cache) - Dramatic effect. Must be skipped in development
+  before_action :set_http_cache_headers, only: [:index]       # quicker next rendering  (if challenged by browser with "If...", it quickly responds with 304 rather than generate a page)
 
   def index
   end
