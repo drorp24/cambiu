9af409847541aa58271fd38db703c4c4f7220564
Dror <drorp24@gmail.com>
2017-09-22 12:10:05 +0300

    Revert "remove 'values' then add it back"
    This reverts commit 2caf8421dda2be4b9a7fa11bdc347df3d0368386.

--- db/migrate/20170922090528_remove_values_from_search.rb
+++
@@ -1,5 +0,0 @@
-class RemoveValuesFromSearch < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :searches, :values, :integer
-  end
-end
--- db/migrate/20170922090818_add_values_to_search_again.rb
+++
@@ -1,5 +0,0 @@
-class AddValuesToSearchAgain < ActiveRecord::Migration[5.0]
-  def change
-    add_column :searches, :values, :integer
-  end
-end
