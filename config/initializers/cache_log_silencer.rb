71330a70657679d0e5c919427044e10f507bf8f7
Dror <drorp24@gmail.com>
2017-09-23 19:48:46 +0300

    silencer

--- config/initializers/cache_log_silencer.rb
+++ config/initializers/cache_log_silencer.rb
@@ -4,14 +4,9 @@ module ActiveSupport
 
       private
 
-      if DalliStore.method_defined? :log
-        alias_method :orig_log, :log
-      end
-
       # silences "Cache read ..." etc debug lines for assets, but allows all others
       def log(operation, key, options=nil)
         return if options.key?(:expires_in)
-        orig_log(operation, key, options)
       end
 
     end
