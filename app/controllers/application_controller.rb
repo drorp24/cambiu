3e56e7c065e968d406d05e942f49dacf81f15a40
Dror <drorp24@gmail.com>
2017-10-23 18:11:48 +0300

    0.9.9.1 bump, start

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,8 @@ class ApplicationController < ActionController::Base
 
   def set_release
     # Important: advance release for *any* deployment to www, or else the old JS/HTML/CSS will be served!
-    @release = '0.9.9'
-    @release_date = Date.new(2017, 10, 18)
+    @release = '0.9.9.1'
+    @release_date = Date.new(2017, 10, 19)
   end
 
   def set_locale
--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,7 +8,7 @@
 console.log('[Serviceworker] here');
 
 //var version;
-var version = '0.9.9';
+var version = '0.9.9.1';
 
 function onInstall(event) {
     console.log('[Serviceworker]', version, "Installing: populating cache with files...");
